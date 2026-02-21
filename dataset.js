87ecb5d74a7ffc7c56317cb51c59c57c672e0b90
btphan27 <150359867+btphan27@users.noreply.github.com>
2026-02-21 00:30:16 -0600

    Tested

---
+++ dataset.js
@@ -0,0 +1,15 @@
+// dataset.js
+const DATASET_SAMPLES = {
+  fake: [
+    {
+      title: "Donald Trump Sends Out Embarrassing New Year’s Eve Message; This is Disturbing",
+    }
+
+  ],
+  real: [
+    {
+      title: "As U.S. budget fight looms, Republicans flip their fiscal script",
+    }
+
+  ]
+};
